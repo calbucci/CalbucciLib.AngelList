1f9a38a51775384662acecbc2bb6140627f0f32d
Marcelo Calbucci <marcelo@calbucci.com>
2016-06-06 19:33:14 -0700

    Validate AngelList link & extract slugs

--- src/CalbucciLib.AngelList/AngelListUtils.cs
+++ src/CalbucciLib.AngelList/AngelListUtils.cs
@@ -9,6 +9,43 @@ namespace CalbucciLib.AngelList
     public static class AngelListUtils
     {
         private static string AngelListBaseUrl = "https://angel.co/";
+
+        private static string[] AngelListLinks = new string[]
+        {
+            "http://angel.co",
+            "https://www.angel.co",
+            "https://angel.co",
+            "http://www.angel.co"
+        };
+
+        private static HashSet<string> _NotValidSlugs;
+
+        static AngelListUtils()
+        {
+            _NotValidSlugs = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase)
+            {
+                "candidates",
+                "jobs",
+                "syndicates",
+                "companies",
+                "people",
+                "investors",
+                "markets",
+                "locations",
+                "salaries",
+                "valuations",
+                "accelerators",
+                "search",
+                "notifications",
+                "messages",
+                "help",
+                "terms",
+                "risks",
+                "news",
+
+            };
+        }
+
         public static string GetSlug(string angelListUrl)
         {
             if (string.IsNullOrWhiteSpace(angelListUrl))
@@ -54,8 +91,74 @@ namespace CalbucciLib.AngelList
                     continue;
                 return false;
             }
+
+            if (_NotValidSlugs.Contains(slug))
+                return false;
+
+
             return true;
             
+        }
+
+        public static bool IsAngelListLink(string link)
+        {
+            if (string.IsNullOrWhiteSpace(link))
+                return false;
+
+            foreach (var prefix in AngelListLinks)
+            {
+                if (link.StartsWith(prefix, StringComparison.CurrentCultureIgnoreCase))
+                    return true;
+            }
+            return false;
+        }
+
+        public static string ExtractSlug(string linkOrSlug)
+        {
+            if (string.IsNullOrWhiteSpace(linkOrSlug))
+                return null;
+
+            linkOrSlug = linkOrSlug.Trim();
+            if (linkOrSlug.StartsWith("@"))
+                linkOrSlug = linkOrSlug.Substring(1);
+
+            string slug = linkOrSlug;
+            if (IsValidSlug(slug))
+                return slug;
+
+            if (linkOrSlug.Length > 16)
+            {
+                // It's probably a link
+                if (!IsAngelListLink(linkOrSlug))
+                    return null;
+
+                try
+                {
+                    Uri link;
+                    if (!Uri.TryCreate(linkOrSlug, UriKind.Absolute, out link))
+                        return null;
+
+                    if (link.Segments.Length == 0)
+                        return null;
+
+                    slug = link.Segments[1];
+                    if (string.IsNullOrEmpty(slug))
+                        return null;
+
+                    if (slug.EndsWith("/"))
+                        slug = slug.Substring(0, slug.Length - 1);
+
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+
+            if (!IsValidSlug(slug))
+                return null;
+
+            return slug;
         }
 
     }
